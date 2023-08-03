cd091b312160273ee0e83f784a1b0cde542b5f7b
V1talquaaa <115567744+V1talquaaa@users.noreply.github.com>
2023-08-03 18:10:14 +0200

    completed hw 3

--- models/contacts.js
+++ models/contacts.js
@@ -2,6 +2,7 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
+
 const contactsPath = path.resolve("./models/contacts.json");
 
 async function listContacts() {
